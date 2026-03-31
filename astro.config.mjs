cc7757c2e74bc5bdc99f9ea92290119d5671d352
rottwelliaz <francisco.flores2101@alumnos.ubiobio.cl>
2026-03-30 23:57:21 -0300

    Actualizando mi pagina web

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   // Reemplaza con tu dominio de GitHub Pages
-  site: 'https://rottwelliaz.github.io', 
+  site: 'https://rottwelliaz.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
